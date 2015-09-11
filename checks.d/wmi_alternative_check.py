113676a08be5d1a621b3e4ac5c3d05af43868653
Yann Mahe <yann@datadoghq.com>
2015-09-11 17:49:14 -0400

    work in progress

--- checks.d/wmi_alternative_check.py
+++ checks.d/wmi_alternative_check.py
@@ -1,8 +1,3 @@
-'''
-Windows Only.
-
-An alternative to Datadog agent WMI check.
-'''
 # 3rd p
 import pywintypes
 from win32com.client import Dispatch
@@ -11,20 +6,22 @@ from win32com.client import Dispatch
 from checks import AgentCheck
 
 
-def WMIAlternativeCheck(AgentCheck):
+class InvalidWMIQuery(Exception):
+    """
+    Invalid WMI Query.
+    """
+    pass
+
+
+class WMIAlternativeCheck(AgentCheck):
+    """
+    An alternative to Datadog agent WMI check.
+
+    Windows only.
+    """
     def __init__(self, name, init_config, agentConfig, instances):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
         self.wmi_conns = {}
-
-    def _get_wmi_conn(self, host, namespace):
-        """
-        Cache connexions
-        """
-        key = "{0}:{1}".format(host, namespace)
-        if key not in self.wmi_conns:
-            server = Dispatch("WbemScripting.SWbemLocator")
-            self.wmi_conns[key] = server.ConnectServer(host, namespace)
-        return self.wmi_conns[key]
 
     def check(self, instance):
         """
@@ -44,35 +41,48 @@ def WMIAlternativeCheck(AgentCheck):
         #
         results = self._query_wmi(w, wmi_class, properties)
 
+    def _get_wmi_conn(self, host, namespace):
+        """
+        Create and cache WMI connexions.
+        """
+        key = "{0}:{1}".format(host, namespace)
+        if key not in self.wmi_conns:
+            server = Dispatch("WbemScripting.SWbemLocator")
+            self.wmi_conns[key] = server.ConnectServer(host, namespace)
+        return self.wmi_conns[key]
+
     def _query_wmi(self, w, wmi_class, wmi_properties):
         """
-
+        Query WMI using WMI Query Language (WQL).
         """
         formated_wmi_properties = ",".join(wmi_properties)
         wql = "Select {0} from {1}".format(formated_wmi_properties, wmi_class)
         self.log.debug(u"Querying WMI: {0}".format(wql))
 
-        res = w.ExecQuery(wql)
+        results = w.ExecQuery(wql)
 
+        try:
+            self._raise_for_invalid(results)
+        except InvalidWMIQuery:
+            self.log.warning(u"Invalid WMI query: {0}".format(wql))
+            res = []
 
         return res
 
-    @staticmethod
-    def _raise_for_invalid(self, result):
+    def _extract_metrics(self, results):
         """
+        Extract, parse metrics from WMI query output and submit them.
+        """
+        for res in results:
+            for wmi_property in res.Properties_:
+                print wmi_property.Name, wmi_property.Value
 
+    @staticmethod
+    def _raise_for_invalid(result):
+        """
+        Raise an InvalidWMIQuery when the result returned by the WMI is invalid.
         """
         try:
             len(result)
-            return True
         except (pywintypes.com_error):
-            self.log.warning(u"Invalid w query")
-            return False
-
-    def _extract_metrics(self):
-        """
-        Extract, parse metrics from WMI query output and submit.
-        """
-        pass
-
-
+            raise InvalidWMIQuery
